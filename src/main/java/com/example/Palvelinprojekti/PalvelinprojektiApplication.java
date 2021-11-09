657d6f452711ffb7218585d7e66474c850984243
Senja Ihalainen <senja.ihalainen@gmail.com>
2021-11-09 15:26:10 +0200

    first commit

---
+++ src/main/java/com/example/Palvelinprojekti/PalvelinprojektiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Palvelinprojekti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalvelinprojektiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalvelinprojektiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Palvelinprojekti/PalvelinprojektiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Palvelinprojekti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalvelinprojektiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
